86de81afb5f3cddb12f4eb02ede9391f1de9d829
AlexCMoreira10 <107936326+AlexCMoreira10@users.noreply.github.com>
2022-08-03 19:26:12 -0300

    mais aulas e testes

---
+++ Curso/aula10/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log(`velocidade do seu carro é ${vel}Km/h`)
+if (vel > 66 && vel) { //condição simples
+    console.log('Você passou acima do velocidade até 20% da velocidade permitida. Multa leve!')
+}
+console.log('Diriga sempre com o cinto de segurança')
---
+++ Curso/aula10/ex008b.js
@@ -0,0 +1,8 @@
+var vel = 80
+console.log(`velocidade do seu carro é ${vel}Km/h`)
+if (vel > 66 && vel < 78) { //condição simples
+    console.log('Você passou acima do velocidade até 20% da velocidade permitida. Multa leve!')
+} else if (vel > 71) {
+    ('Você passou acima de 20% da velocidade permitida. Multa Grave!')
+}
+console.log('Diriga sempre com o cinto de segurança')
