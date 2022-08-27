c32ba1c778f0e28410f2605b4956de14e59e22a9
AlexCMoreira10 <107936326+AlexCMoreira10@users.noreply.github.com>
2022-08-26 22:10:31 -0300

    estudo de arrays

---
+++ Curso/aula15/ambiente.js
@@ -0,0 +1,15 @@
+/*
+let num = ['Clara','Luiza', 'Ana','Fernanda','Bruna', 'Any']
+num.push('clauida')
+num.sort()
+//console.log(`os intens do velor é [${num}]`) 
+//console.log(`possui o total de ${num.length} elementos `) 
+
+for (let pos=0; pos < num.length; pos++ ) {
+    console.log(`Mostrando Vetores ${num[pos]}`)
+} */
+
+let num = [5,6,7,8,9]
+for (let pos in num ) {
+    console.log(`Mostrando Vetores ${num[pos]}`)
+}
